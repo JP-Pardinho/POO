e5058af54d5f712488e0fee6a690a8112b63c1b2
JP-Pardinho <jppardinhor@gmail.com>
2025-05-09 23:27:07 -0300

    Refazer organização das pastas

--- aula-001/src/main/java/com/example/Main.java
+++ aula-001/src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         int a, b;
 
         Scanner scan = new Scanner(System.in);
